9c7acf64ef91b478e2e31b07b8c730c03c60808d
Haken <JJanik2k@gmail.com>
2023-02-18 10:53:23 +0100

    quiz completed

---
+++ src/components/Result.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { defineProps } from 'vue';
+import { RouterLink } from 'vue-router';
+
+const { quizQuestionLength, numberOfCorrenctAnswers } = defineProps([
+    'quizQuestionLength',
+    'numberOfCorrenctAnswers',
+]);
+</script>
+
+<template>
+    <div class="results">
+        <p>Your results...</p>
+        <h1>{{ numberOfCorrenctAnswers }}/{{ quizQuestionLength }}</h1>
+        <RouterLink to="/">Go Back</RouterLink>
+    </div>
+</template>
+
+<style scoped>
+.results {
+    text-align: center;
+    padding: 100px 0px;
+}
+
+p {
+    font-size: 25px;
+}
+
+h1 {
+    font-size: 80px;
+}
+</style>
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,5 +1,62 @@
+<script setup>
+import Question from '../components/Question.vue';
+import QuizHeader from '../components/QuizHeader.vue';
+import Result from '../components/Result.vue';
+import { useRoute } from 'vue-router';
+import { ref, watch, computed } from 'vue';
+import quizzes from '../data/quizzes.json';
+
+const route = useRoute();
+const quizId = parseInt(route.params.id);
+const quiz = quizzes.find((q) => q.id === quizId);
+const currentQuestionIndex = ref(0);
+const numberOfCorrenctAnswers = ref(0);
+const showResults = ref(false);
+
+// Zmiana numerków pytań
+const questionStatus = computed(
+    () => `${currentQuestionIndex.value}/${quiz.questions.length}`
+);
+
+// Zmiana paska postępu
+const barPercentage = computed(() => {
+    return `${(currentQuestionIndex.value / quiz.questions.length) * 100}%`;
+});
+
+const onOptionSelected = (isCorrect) => {
+    if (isCorrect) {
+        numberOfCorrenctAnswers.value++;
+    }
+
+    if (quiz.questions.length - 1 === currentQuestionIndex.value) {
+        showResults.value = true;
+    }
+
+    currentQuestionIndex.value++;
+};
+</script>
+
 <template>
-  <div>
-    <h1>Hello</h1>
-  </div>
+    <div>
+        <QuizHeader
+            :questionStatus="questionStatus"
+            :barPercentage="barPercentage"
+        />
+
+        <div>
+            <Question
+                v-if="!showResults"
+                :question="quiz.questions[currentQuestionIndex]"
+                @selectOption="onOptionSelected"
+            />
+
+            <Result
+                v-else
+                :quizQuestionLength="quiz.questions.length"
+                :numberOfCorrenctAnswers="numberOfCorrenctAnswers"
+            />
+        </div>
+    </div>
 </template>
+
+<style scoped></style>
